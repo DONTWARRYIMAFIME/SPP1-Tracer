f3e72373bf962c3095e13751de3a34c82df8294f
Kostyukovich_V_A <demon4ik228228@gmail.com>
2021-09-14 14:18:45 +0300

    Initial commit

---
+++ SPP1-Tracer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SPP1_Tracer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
